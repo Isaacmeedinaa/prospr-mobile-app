b44058a9395cddd9ea765b7b23abce7a6dccdef7
Isaac Medina <isaac.meedinaa@gmail.com>
2020-08-27 19:57:32 -0500

    set up redux

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import {} from "react-native";
+import { Provider } from "react-redux";
+import store from "./app/store/store";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
 
 export default App;
